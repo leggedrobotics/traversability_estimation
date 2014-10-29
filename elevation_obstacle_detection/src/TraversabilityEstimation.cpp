fc0d9a04402567eb99121ad357592011c38c4daf
Peter Fankhauser <pfankhauser@ethz.ch>
2014-10-29 21:05:03 +0100

    Refactoring.

---
+++ elevation_obstacle_detection/src/TraversabilityEstimation.cpp
@@ -0,0 +1,122 @@
+/*
+ * TraversabilityEstimation.cpp
+ *
+ *  Created on: Oct 22, 2014
+ *      Author: Ralf Kaestner, Peter Fankhauser
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#include "traversability_estimation/TraversabilityEstimation.hpp"
+
+// Grid Map
+#include <grid_map_msg/GetGridMap.h>
+
+//STL
+#include <deque>
+
+using namespace std;
+using namespace nav_msgs;
+using namespace grid_map_msg;
+
+namespace traversability_estimation {
+
+TraversabilityEstimation::TraversabilityEstimation(ros::NodeHandle& nodeHandle)
+    : nodeHandle_(nodeHandle),
+      traversabilityType_("traversability")
+{
+  ROS_INFO("Traversability estimation node started.");
+
+  readParameters();
+  submapClient_ = nodeHandle_.serviceClient<GetGridMap>(submapServiceName_);
+  occupancyGridPublisher_ = nodeHandle.advertise<OccupancyGrid>("traversability_map", 1, true);
+  updateTimer_ = nodeHandle_.createTimer(updateDuration_,
+                                         &TraversabilityEstimation::updateTimerCallback, this);
+
+  requestedMapTypes_.push_back("elevation");
+  requestedMapTypes_.push_back("surface_normal_z");
+}
+
+TraversabilityEstimation::~TraversabilityEstimation()
+{
+  updateTimer_.stop();
+  nodeHandle_.shutdown();
+}
+
+bool TraversabilityEstimation::readParameters()
+{
+  nodeHandle_.param("submap_service", submapServiceName_, string("/get_grid_map"));
+
+  double updateRate;
+  nodeHandle_.param("update_rate", updateRate, 1.0);
+  updateDuration_.fromSec(1.0 / updateRate);
+
+  nodeHandle_.param("map_frame_id", mapFrameId_, string("map"));
+  string robotFrameId;
+  nodeHandle_.param("robot_frame_id", robotFrameId, string("robot"));
+  double mapCenterX, mapCenterY;
+  nodeHandle_.param("map_center_x", mapCenterX, 0.0);
+  nodeHandle_.param("map_center_y", mapCenterY, 0.0);
+  submapPoint_.header.frame_id = robotFrameId;
+  submapPoint_.point.x = mapCenterX;
+  submapPoint_.point.y = mapCenterY;
+  submapPoint_.point.z = 0.0;
+
+  nodeHandle_.param("map_length_x", mapLength_.x(), 5.0);
+  nodeHandle_.param("map_length_y", mapLength_.y(), 5.0);
+  return true;
+}
+
+void TraversabilityEstimation::updateTimerCallback(const ros::TimerEvent& timerEvent)
+{
+  grid_map_msg::GridMap mapMessage;
+  if (getGridMap(mapMessage)) {
+    grid_map::GridMap map(mapMessage);
+    computeTraversability(map);
+    publishAsOccupancyGrid(map);
+  } else {
+    ROS_WARN("Failed to retrieve elevation grid map.");
+  }
+}
+
+bool TraversabilityEstimation::getGridMap(grid_map_msg::GridMap& map)
+{
+  submapPoint_.header.stamp = ros::Time(0);
+  geometry_msgs::PointStamped submapPointTransformed;
+
+  try {
+    transformListener_.transformPoint(mapFrameId_, submapPoint_, submapPointTransformed);
+  } catch (tf::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+  }
+
+  GetGridMap submapService;
+  submapService.request.positionX = submapPointTransformed.point.x;
+  submapService.request.positionY = submapPointTransformed.point.y;
+  submapService.request.lengthX = mapLength_.x();
+  submapService.request.lengthY = mapLength_.y();
+  submapService.request.dataDefinition.resize(requestedMapTypes_.size());
+
+  for (unsigned int i = 0; i < requestedMapTypes_.size(); ++i) {
+    submapService.request.dataDefinition[i].data = requestedMapTypes_[i];
+  }
+
+  if (!submapClient_.call(submapService)) return false;
+  map = submapService.response.gridMap;
+  return true;
+}
+
+void TraversabilityEstimation::computeTraversability(grid_map::GridMap& elevationMap)
+{
+  // TODO
+  elevationMap.add(traversabilityType_, elevationMap.get("surface_normal_z"));
+}
+
+void TraversabilityEstimation::publishAsOccupancyGrid(const grid_map::GridMap& map) const
+{
+  OccupancyGrid occupancyGrid;
+  // This flips data from traversability to occupancy.
+  map.toOccupancyGrid(occupancyGrid, traversabilityType_, 1.0, 0.0);
+  occupancyGridPublisher_.publish(occupancyGrid);
+}
+
+} /* namespace */
