6a90ca017c6d67d2bc30f7ffcd7d49c9e5710d85
martiwer <martiwer@student.ethz.ch>
2015-03-12 10:19:09 +0100

    Added comments

--- libtraversability_estimation/include/filters/SlopeFilter.h
+++ libtraversability_estimation/include/filters/SlopeFilter.h
@@ -1,3 +1,11 @@
+/*
+ * SlopeFilter.h
+ *
+ *  Created on: Mar 12, 2015
+ *      Author: Martin Wermelinger
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
 #ifndef SLOPEFILTER_H
 #define SLOPEFILTER_H
 #include <stdint.h>
@@ -14,14 +22,37 @@
 
 namespace filters {
 
+/*!
+ * Slope Filter class to compute the slope danger value of an elevation map.
+ */
 template <typename T>
 class SlopeFilter: public FilterBase<T>
 {
-public:
 
+public:
+  /*!
+   * Constructor
+   */
   SlopeFilter();
+
+  /*!
+  * Destructor.
+  */
   virtual ~SlopeFilter();
+
+  /*!
+   * Configures the filter from parameters on the Parameter Server
+   */
   virtual bool configure();
+
+  /*!
+   * Computes the slope danger value based on an elevation map
+   * The slope danger value is set between 0.0 and 1.0, where a value of 0.0 means fully
+   * traversable and 1.0 means barely traversable. NAN indicates that the terrain
+   * is not traversable.
+   * @param elevationMap the map for which the slope danger value is computed.
+   * @param slope_map gridMap with slope danger value.
+   */
   virtual bool update(const T & elevation_map, T& slope_map);
 
 private:
