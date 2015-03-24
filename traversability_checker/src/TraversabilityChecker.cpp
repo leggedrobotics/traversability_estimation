b002dba03c4842a3f6f8f59773c2cd3f102043a3
Peter Fankhauser <pfankhauser@ethz.ch>
2015-03-24 20:45:46 +0100

    Added msgs/srvs and structure for traversability checker.

---
+++ traversability_checker/src/TraversabilityChecker.cpp
@@ -0,0 +1,24 @@
+/*
+ * TraversabilityChecker.cpp
+ *
+ *  Created on: Mar 24, 2015
+ *      Author: Péter Fankhauser
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#include <traversability_checker/TraversabilityChecker.hpp>
+
+namespace traversability_checker {
+
+TraversabilityChecker::TraversabilityChecker()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+TraversabilityChecker::~TraversabilityChecker()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace traversability_checker */
---
+++ traversability_checker/src/traversability_checker_node.cpp
@@ -0,0 +1,18 @@
+/*
+ * traversability_checker_node.cpp
+ *
+ *  Created on: Mar 24, 2014
+ *      Author: Peter Fankhauser
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#include <ros/ros.h>
+#include "traversability_checker/TraversabilityChecker.hpp"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "traversability_checker");
+  ros::NodeHandle nodeHandle("~");
+//  traversability_checker::TraversabilityChecker traversabilityChecker(nodeHandle);
+  ros::spin();
+  return 0;
+}
