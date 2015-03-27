60ec54e98183db693ffa1f6deee000cd4d9ffbf9
Peter Fankhauser <pfankhauser@ethz.ch>
2015-03-27 01:16:59 +0100

    Rough structure ok.

--- traversability_checker/include/traversability_checker/TraversabilityChecker.hpp
+++ traversability_checker/include/traversability_checker/TraversabilityChecker.hpp
@@ -10,6 +10,8 @@
 
 // ROS
 #include <ros/ros.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/TwistWithCovarianceStamped.h>
 
 // STD
 #include <string>
@@ -23,13 +25,23 @@ class TraversabilityChecker
   virtual ~TraversabilityChecker();
   bool readParameters();
   void check(const ros::TimerEvent& timerEvent);
+  void updateRobotPose(const geometry_msgs::PoseWithCovarianceStamped& pose);
+  void updateRobotTwist(const geometry_msgs::TwistWithCovarianceStamped& twist);
 
  private:
   ros::NodeHandle nodeHandle_;
+  ros::Publisher safetyPublisher_;
   ros::Timer timer_;
   ros::Duration timerDuration_;
   ros::ServiceClient serviceClient_;
   std::string serviceName_;
+  ros::Subscriber robotPoseSubscriber_;
+  std::string robotPoseTopic_;
+  ros::Subscriber robotTwistSubscriber_;
+  std::string robotTwistTopic_;
+  double extrapolationDuration_;
+  geometry_msgs::Pose robotPose_;
+  geometry_msgs::Twist robotTwist_;
 };
 
 } /* namespace traversability_checker */
--- traversability_checker/src/TraversabilityChecker.cpp
+++ traversability_checker/src/TraversabilityChecker.cpp
@@ -16,23 +16,24 @@ TraversabilityChecker::TraversabilityChecker(const ros::NodeHandle& nodeHandle)
     : nodeHandle_(nodeHandle)
 {
   readParameters();
+//  safetyPublisher_ = nodeHandle_.advertise<???>("is_safe", 1);
   timer_ = nodeHandle_.createTimer(timerDuration_, &TraversabilityChecker::check, this);
   serviceClient_ = nodeHandle_.serviceClient<traversability_msgs::CheckFootprintPath>(serviceName_, true);
+  robotPoseSubscriber_ = nodeHandle_.subscribe(robotPoseTopic_, 1, &TraversabilityChecker::updateRobotPose, this);
+  robotTwistSubscriber_ = nodeHandle_.subscribe(robotTwistTopic_, 1, &TraversabilityChecker::updateRobotTwist, this);
 }
 
 TraversabilityChecker::~TraversabilityChecker()
 {
 }
 
-void TraversabilityChecker::check(const ros::TimerEvent&)
-{
-  ROS_DEBUG("Checking for traversability.");
-}
-
 bool TraversabilityChecker::readParameters()
 {
   nodeHandle_.param("service_to_call", serviceName_,
                     std::string("/traversability_estimation/check_footprint_path"));
+  nodeHandle_.param("robot_pose_topic", robotPoseTopic_, std::string("/state_estimator/pose"));
+  nodeHandle_.param("robot_twist_topic", robotTwistTopic_, std::string("/state_estimator/twist"));
+  nodeHandle_.param("extrapolation_duration", extrapolationDuration_, 1.0);
   double rate;
   nodeHandle_.param("rate", rate, 2.0);
   timerDuration_.fromSec(1.0 / rate);
@@ -40,5 +41,47 @@ bool TraversabilityChecker::readParameters()
   return true;
 }
 
+void TraversabilityChecker::check(const ros::TimerEvent&)
+{
+  // TODO Make sure robotPose_ and robotTwist_ were already received.
+  ROS_DEBUG("Checking for traversability.");
+  geometry_msgs::Pose endPose;
+  // TODO Add frame checking and conversion.
+  // TODO Include rotation.
+  endPose.position.x = robotPose_.position.x + extrapolationDuration_ * robotTwist_.linear.x;
+  endPose.position.y = robotPose_.position.y + extrapolationDuration_ * robotTwist_.linear.y;
+  endPose.position.z = robotPose_.position.z + extrapolationDuration_ * robotTwist_.linear.z;
+  traversability_msgs::CheckFootprintPath check;
+  auto& path = check.request.path;
+  path.poses.header.stamp = ros::Time::now();
+  path.poses.header.frame_id = "map"; // TODO
+  path.poses.poses.push_back(robotPose_); // Start pose.
+  path.poses.poses.push_back(endPose);
+  path.radius = 0.5; // TODO
+
+  ROS_DEBUG("Sending request to %s.", serviceName_.c_str());
+  if (!serviceClient_.call(check)) {
+    ROS_ERROR("Failed to call service %s.", serviceName_.c_str());
+    return;
+  }
+
+//  safetyPublisher_.publish(); // TODO.
+
+  ROS_DEBUG("Checking for traversability.");
+}
+
+void TraversabilityChecker::updateRobotPose(
+    const geometry_msgs::PoseWithCovarianceStamped& pose)
+{
+  robotPose_ = pose.pose.pose;
+  ROS_DEBUG("Updated robot pose.");
+}
+
+void TraversabilityChecker::updateRobotTwist(
+    const geometry_msgs::TwistWithCovarianceStamped& twist)
+{
+  robotTwist_ = twist.twist.twist;
+  ROS_DEBUG("Updated robot twist.");
+}
 
 } /* namespace traversability_checker */
