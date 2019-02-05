7017ed5709bfda55540f9909934800b06bfeb5d9
Marco Tranzatto <marcot@ethz.ch>
2019-02-05 12:04:07 +0100

    Function to check if 2D pose belongs to current traversability map.

--- traversability_estimation/include/traversability_estimation/TraversabilityMap.hpp
+++ traversability_estimation/include/traversability_estimation/TraversabilityMap.hpp
@@ -150,6 +150,15 @@ class TraversabilityMap
     */
   void restoreDefaultTraversabilityUnknownRegionsReadAtInit();
 
+  /*!
+   * Checks if given 2D pose (x,y) belongs to the traversability layer of the current map.
+   * @param x X coordinate of the 2D pose to check.
+   * @param y Y coordinate of the 2D pose to check.
+   * @return true if pose belongs to the traversability layer of the current map,
+   *              i.e. if there is a valid traversability value in that position.
+   */
+  bool is2DPoseInTraversabilityMap(double x, double y) const;
+
  private:
 
   /*!
